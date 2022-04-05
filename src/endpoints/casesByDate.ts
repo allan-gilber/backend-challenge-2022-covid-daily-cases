03baaa0d860a6cbbf075e11b495181b3b45a931d
allan-gilber <allangilber@yahoo.com.br>
2022-04-05 17:34:58 -0300

    feat: created error handling logic for the second endpoint.

--- src/endpoints/casesByDate.ts
+++ src/endpoints/casesByDate.ts
@@ -1,3 +1,4 @@
+import { isValid } from 'date-fns';
 import { Request, Response } from 'express';
 import connection from '../connection';
 import {  covidDatabaseResponseStructure } from '../typesAndIntefaces';
@@ -5,24 +6,27 @@ import {  covidDatabaseResponseStructure } from '../typesAndIntefaces';
 export default async function casesByDate (
 	req: Request,
 	res: Response
-): Promise<any> {
+): Promise<void> {
 	try {
 		const { date } = req.params;
-		const timestamp: any = Date.parse( date );
 
-		if (date.length !== 10&& isNaN(timestamp) === false ){
-			return res.status(500).send({message: 'Error: Please send a valid date!'});
+		if (typeof date !== 'string' || date.length !== 10 || !isValid(new Date(date))){
+			throw 'invalidDate';
 		}
-
 		await connection.raw(`
-		SELECT  location, variant, num_sequences as "numberOfSequences", perc_sequences as "percentageOfSequences", num_sequences_total as "totalOfSequencesNumbers" 
-		FROM covid_cases_by_date 
-		WHERE covid_cases_by_date.date = "${date}" 
-		GROUP BY location, variant
-		ORDER BY covid_cases_by_date.location ASC;
+			SELECT  location, variant, num_sequences as "numberOfSequences", perc_sequences as "percentageOfSequences", num_sequences_total as "totalOfSequencesNumbers" 
+			FROM covid_cases_by_date 
+			WHERE covid_cases_by_date.date = "${date}" 
+			GROUP BY location, variant
+			ORDER BY covid_cases_by_date.location ASC;
 		`)
 			.then((data:[covidDatabaseResponseStructure[]]) => {
 				const finalObject: any = {} ;
+
+				if(!data[0].length){
+					throw 'notFound';
+				}
+
 				data[0].map((caseData) => {
 					const nameOfLocation: string = caseData.location;
 					const nameOfVariant: string = caseData.variant;
@@ -42,9 +46,21 @@ export default async function casesByDate (
 					};
 					
 				});
-				res.status(200).send([finalObject]);
+				res.status(200).send({data: [finalObject]});
 			});
 	} catch (error){
-		res.status(500).send(`Error: ${error}`);
+		console.log('casesByDate error: ', error);
+		if(error === 'notFound'){
+			console.log('casesByDate: nothing was found');
+			res.status(404).send({message: 'No data was found for the requested date. Please try another date.'});
+			return;
+		}
+		if(error === 'invalidDate'){
+			console.log('casesByDate: a invalid date was provided');
+			res.status(400).send({message: 'Please send a valid dateformat (YYYY/MM/DD)'});
+			return;
+		}
+		console.log('casesByDate error: the server didnt know how to handle the error.');
+		res.status(500).send({message: 'Internal server error'});
 	}
 }
