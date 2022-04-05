28abeeb32cebf9e8d089f373fb0bf7ef37e87dfe
allan-gilber <allangilber@yahoo.com.br>
2022-04-05 16:03:10 -0300

    fix: padronization of return messages to be a property 'message' from a object..

--- src/endpoints/serverStatus.ts
+++ src/endpoints/serverStatus.ts
@@ -4,5 +4,5 @@ export default async function createUser (
 	req: Request,
 	res: Response
 ): Promise<void> {
-	res.status(200).send('Backend Challenge 2021 🏅 - Covid Daily Cases');
+	res.status(200).send({message: 'Backend Challenge 2021 🏅 - Covid Daily Cases'});
 }
